2a105c9bd3060e2f3e0415ac322daec2250faf0f
mishrashivang <85113719+mishrashivang@users.noreply.github.com>
2021-05-31 18:19:47 +0530

    php form

---
+++ firstform.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<tittle><b>registration</b></tittle>
+</head>
+<body>
+<form action="#" method= "POST">
+    <pre>
+      <b> NAME:</b>    <input type="text" name="name" value="ENTER NAME HERE"><br>
+    <b> GENDER: </b>   <input type="radio" name="gen">male
+                <input type="radio" name"gen">female<br>
+        <b> DOB:</b>   <input type="date" name="dob"><br>
+    <b> mobile :</b>   <input type="number" name="mobile"><br>
+   <b> PASSWORD:</b>   <input type="password" name="password"><br>
+                <input type="submit" name"submit" value="sign up">
+                </pre>
+    
+</form>
+</body>
+</html>
